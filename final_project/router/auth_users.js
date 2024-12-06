1639d5857c6071e30f921d8c50c7d8efbdc58cce
Sajid Hussein <121464156+Hackingsimulator@users.noreply.github.com>
2024-12-05 18:51:25 -0800

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,47 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+    for(let user in users){
+        if (user.username === username) {
+            return false;
+        }
+        return true;
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    for(let user in users){
+        if(user.username != username){
+            return false
+        }
+        if(user.password != password){
+            return false
+        }
+    }
+    return true
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {username, password} = req.body;
+  if(authenticatedUser(username, password)){
+    const token = jwt.sign({ username }, "fingerprint_customer", { expiresIn: '1h' });
+
+    // Store the token in the session
+    if (!req.session.authorization) {
+      req.session.authorization = {};
+    }
+    req.session.authorization.accessToken = token;
+  
+    // Return success response
+    return res.status(201).json({ 
+      message: "User logged in successfully", 
+      token: token 
+    });
+  }
+  return res.status(410).json({message: "Wrong Email or Password"});
 });
 
 // Add a book review
